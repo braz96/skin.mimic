8a12945802e59e290f8561664da1b06e480b5bbc
bryanbrazil <bryandbrazil@gmail.com>
2015-06-07 10:39:18 -0700

    Add skin settings to export/import skin settings

--- extras/yesno.py
+++
@@ -1,9 +0,0 @@
-import xbmc
-import xbmcgui
-
-dialog = xbmcgui.Dialog()
-i = dialog.yesno("Reset Skin Settings", "Are you sure you want to reset all skin settings? Caution, this will reset all menus, backgrounds, and widgets to their default setting.")
-
-if i == 1:
-    xbmc.executebuiltin("Skin.ResetSettings")
-    xbmc.executebuiltin("Skin.SetBool(Enable.HomeDefault)")
